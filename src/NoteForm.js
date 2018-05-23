b653203096f049efb70d185e8ca0a441c58f4f29
Rodrigo Gerbasi <rgerbasi98@gmail.com>
2018-05-23 09:56:39 -0400

    Style noteform and notelist after adding them

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import './NoteForm.css'
+
 const NoteForm = () => {
     return (
-        <div className="Noteform">
-            Noteform
+        <div className="NoteForm">
+          <div className="form-actions">
+            <button type="button">
+              <i className="far fa-trash-alt"></i>
+            </button>
+          </div>
+          <form>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+              />
+            </p>
+            
+            <textarea name="body"></textarea>
+          </form>
         </div>
     )
 }
